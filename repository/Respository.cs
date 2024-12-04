0791a9747efdffbc4b7a0d6e651d695c4ddd4244
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-04 19:17:55 +0100

    repository

---
+++ repository/Respository.cs
@@ -0,0 +1,78 @@
+using System.ComponentModel;
+
+namespace Repository;
+
+public class EntityBase{
+    public Guid Id {get;init;}
+
+    public EntityBase(Guid id) => Id = id;
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is EntityBase entityBase){
+            return entityBase.Id == Id;
+        }
+        return false;
+    }
+    public override int GetHashCode(){
+        return Id.GetHashCode();
+    }
+
+}
+
+public interface IDatabase<T> where T:EntityBase{
+    ISet<T> Data{get;}
+}
+public interface IAdd<T>:IDatabase<T> where T:EntityBase{
+    void Add(T entity){
+        Data.Add(entity);
+    }
+}
+public interface IGet<T,ID>:IDatabase<T> where T:EntityBase{
+    T Get(ID id){
+        var entity = Data.Where(x => x.Id.Equals(id)).FirstOrDefault();
+        if(entity == null){
+            throw new Exception("");
+        }
+        return entity;
+    }
+}
+public interface IUpdate<T,ID>:IGet<T,ID> where T:EntityBase{
+    void Update(T entity){
+        Data.Remove(entity);
+        Data.Add(entity);
+    }   
+}
+public interface IRemove<T,ID>:IGet<T,ID> where T:EntityBase{
+    void Remove(T entity){
+        Data.Remove(entity);
+    }
+}
+
+public interface IReposiroty<T,ID>:IAdd<T>,IUpdate<T,ID>,IRemove<T,ID> where T:EntityBase{
+
+}
+
+public class Customer : EntityBase
+{
+    public Customer(Guid id) : base(id)
+    {
+    }
+}
+public class User : EntityBase
+{
+    public User(Guid id) : base(id)
+    {
+    }
+}
+
+public class ServiceCustomer : IReposiroty<Customer, Guid>
+{
+    private static ISet<Customer> customers = new HashSet<Customer>(); 
+    public ISet<Customer> Data => customers;
+}
+public class ServiceUSer : IGet<User, Guid>
+{
+    private static ISet<User> users = new HashSet<User>(); 
+    public ISet<User> Data => users;
+}
